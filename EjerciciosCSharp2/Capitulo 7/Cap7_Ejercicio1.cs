b1bc3c5ef7091823f4fdf4140f05a4586e2af2b2
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-06 15:13:38 -0400

    Agregando cap7 ejercicio 1

---
+++ EjerciciosCSharp2/Capitulo 7/Cap7_Ejercicio1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp2.Capitulo_7
+{
+    public class Cap7_Ejercicio1
+    {
+        public static void CalPromedio()
+        {
+
+            int num;
+            Console.WriteLine("Ingrese Numero de Calificaciones a calcular ");
+            num = int.Parse(Console.ReadLine());
+            int s = 0;
+            int[] cali = new int[num];
+
+
+            for (int i = 0; i < cali.Length; i++)
+            {
+                Console.WriteLine("Ingrese Calificacion de estudiante ");
+                cali[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int id = 0; id < cali.Length; id++)
+            {
+                Console.WriteLine("/n Calificacion {0}: {1}", id, cali[id]);
+                s += cali[id];
+            }
+
+            Console.WriteLine("Su suma es " + s);
+            Console.WriteLine("Promedio de calificacion es " + s / cali.Length);
+
+            Array.Sort(cali);
+            Console.WriteLine("Calificacion minima es: " + cali[0]);
+            Array.Reverse(cali);
+            Console.WriteLine("Calificacion maxima es: " + cali[0]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
