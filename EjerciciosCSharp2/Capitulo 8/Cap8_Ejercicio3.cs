bab95f6938565510d28c26d680c4cc0637e9c3e6
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-06 15:23:00 -0400

    Agregando Cap8 Ejercicio 3

---
+++ EjerciciosCSharp2/Capitulo 8/Cap8_Ejercicio3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp2.Capitulo_8
+{
+    public class Cap8_Ejercicio3
+    {
+        public static void FormatoHora()
+        {
+            Console.WriteLine(string.Format("{0:hh:mm:ss tt}", DateTime.Now));
+
+
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+
+            Console.ReadKey();
+        }
+    }
+}
+
