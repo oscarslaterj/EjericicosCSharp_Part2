ea8b814e874e648afb88f10bbf9eac2a47a0b6a6
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-06 15:29:21 -0400

    Ejercicios Terminados

---
+++ EjerciciosCSharp2/Capitulo 8/Cap8_Ejercicio5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp2.Capitulo_8
+{
+    public class Cap8_Ejercicio5
+    {
+        public static void OrdenAlfabetico()
+        {
+
+            {
+                string[] cadenas = { "casa", "motorizado", "programador", "Aplicada" };
+
+                Console.WriteLine("\t \t Palabras desordenadas:");
+                foreach (string s in cadenas)
+                    Console.WriteLine(s);
+
+                Console.WriteLine("\n  \t \t Palabras ordenadas:");
+                foreach (string s in cadenas)
+                    Console.WriteLine(s);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
