701de325fe7a166741552ded524cebfc97bf51e9
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-06 15:07:09 -0400

    Termiando Cap5 Ejercicio 4

---
+++ EjerciciosCSharp2/Capitulo 5/Cap5_Ejercicio4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp2.Capitulo_5
+{
+    public class Cap5_Ejercicio4
+    {
+        public static void CalFactorial()
+        {
+            int num;
+            Console.WriteLine("\t \t \t Factorial de un número \n");
+        do
+            {
+                Console.Write("Ingrese un número ");
+                num = int.Parse(Console.ReadLine( ));
+
+        } while (num< 0);
+
+                factorial(num);
+        Console.ReadKey();
+            }
+
+
+    static void factorial(int n)
+    {
+        if (n < 0)
+        {
+            Console.Write("\n Ingrese un numero positivo \n");
+        }
+
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+        Console.WriteLine("\nEl Factorial de " + n + " es: " + fact);
+        }
+    }
+}
